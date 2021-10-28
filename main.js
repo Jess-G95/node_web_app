cf1f68775e68fc4015e3364a9cc889a78d79bece
Jess-G95 <79566501+Jess-G95@users.noreply.github.com>
2021-10-28 16:33:57 +0100

    api

--- main.js
+++ main.js
@@ -1,14 +1,6 @@
-// const express = require('express')
-// const app = express();
-// const port = 3000;
-
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// });
-
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}!`)
-// });
+const express = require('express')
+const app = express();
+const port = 3000;
 
 let quotesDiv = document.getElementById('quotes');
 fetch('https://api.kanye.rest')
@@ -16,3 +8,11 @@ fetch('https://api.kanye.rest')
 .then(quote => {
     quotesDiv.innerHTML += `<p> ${quote.quote} </p>`
 })
+
+app.get('/', (req, res) => {
+  res.send(quotesDiv)
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`)
+});
